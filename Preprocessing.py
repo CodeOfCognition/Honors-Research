6bb60974b158344b2c2b3c750bd541fd3fbf6185
CodeOfCognition <59781260+CodeOfCognition@users.noreply.github.com>
2021-08-20 15:32:47 -0400

    Preprocesing Finalized (for now: Aug 20)

--- Preprocessing.py
+++ Preprocessing.py
@@ -8,7 +8,7 @@ import numpy as np
 
 start_time = time.time()
 
-def genVectorWords(corporaDir, n, includeStopWords):
+def genVectorWords(prePath, corporaDir, n, includeStopWords):
 
     def runFile(fileName):
         with open(fileName, "rt") as f:
@@ -44,12 +44,12 @@ def genVectorWords(corporaDir, n, includeStopWords):
 
     vector_words = FreqDist()
     i = 1
-    for filename in os.listdir('./corpora/' + corporaDir):
+    for filename in os.listdir(prePath + corporaDir):
             if filename.endswith(".txt"):
                 if (i%10 == 0):
                     print("--- %s seconds ---" % (time.time() - start_time))
                     print ("Running file " + str(i) + ": " + filename)
-                runFile('./corpora/' + corporaDir + '/' + filename)
+                runFile(prePath + corporaDir + '/' + filename)
                 i += 1
     if not includeStopWords:
         toRemove = list()
@@ -65,29 +65,25 @@ def genVectorWords(corporaDir, n, includeStopWords):
 
     most_common = vector_words.most_common(n)
     sorted_vector_words = sorted(vector_words.items(), key=lambda x: x[1], reverse=True)
-    try:
-        f = open("./helperFiles/vector_words_" + str(n) + "_derived_" + str(corporaDir), "x")
-    except:
-        f = open("./helperFiles/vector_words_" + str(n) + "_" + str(corporaDir), "w")
+    if not includeStopWords:
+        f = open("./helperFiles/vector_words_" + str(n) + "_derived_" + str(corporaDir) + ".txt", "w")
+    else:
+        f = open("./helperFiles/vector_words_" + str(n) + "_derived_" + str(corporaDir) + "_stops.txt", "w")
     i = 1
     for pair in sorted_vector_words:
         if i > n:
             break
         f.write(pair[0] + '\n')
         i += 1
+    f.close()
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
-def cleanCorpora(corporaDir, vectorWordsFile, includeStopWords):
-    if not os.path.isdir("./corpora/" + corporaDir + "_clean"):
-        os.mkdir("./corpora/" + corporaDir + "_clean")
-    with open("./helperFiles/" + vectorWordsFile + ".txt", "rt") as f:
-        fin = f.read()
-        vector_words = set(fin.split())
-    f.close
+def cleanCorpora(prePath, corporaDir, vectorWordsFile, includeStopWords):
+    
 
     def runFile(corporaDir, filename):
-        with open("/volumes/Robbie_External_Hard_Drive/" + corporaDir + "/" + filename, "rt") as f:
+        with open(prePath + corporaDir + "/" + filename, "rt") as f:
             fin = f.read()
 
             entry_pat = "\*--\s(.*?)--\*" # Separates entries
@@ -117,14 +113,33 @@ def cleanCorpora(corporaDir, vectorWordsFile, includeStopWords):
                 # print("Comment length: " + str(commentWC[i]))
         f.close
 
+        with open("./helperFiles/stopList.txt", "rt") as f:
+            fin = f.read()
+            stopWords = set(fin.split())
+        f.close 
      
+        if includeStopWords:
+            cleanComments = list()
+            for i in range(len(rawComments)):
+                cleanComments.append([])
+            punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+            for i in range(len(rawComments)):
+                for word in rawComments[i].split():
+                    if word.lower() in vector_words:
+                        cleanComments[i].append(word.lower())
+                    else:
+                        for letter in word:
+                                if letter in punc: 
+                                    word = word.replace(letter, "") 
+                        if word in vector_words:
+                            cleanComments[i].append(word)
 
-        if not includeStopWords:
-            with open("./helperFiles/stopList.txt", "rt") as f:
-                fin = f.read()
-                stopWords = set(fin.split())
-            f.close 
+            data = zip(times, subreddits, (cleanComments))
+            f = open("corpora/" + corporaDir + "_clean_stops/" + filename + ".csv", "w")
+            writer = csv.writer(f)
+            writer.writerows(data)
 
+        else:
             cleanComments = list()
             for i in range(len(rawComments)):
                 cleanComments.append([])
@@ -141,37 +156,30 @@ def cleanCorpora(corporaDir, vectorWordsFile, includeStopWords):
                                     word = word.replace(letter, "") 
                         if word in vector_words:
                             cleanComments[i].append(word)
-
-
-
-        else:
-            cleanComments = list()
-            punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-            for i in range(len(rawComments)):
-                for word in rawComments[i].split():
-                    word = word.lower()
-                    if word in vector_words:
-                        cleanComments[i].append(word)
-                    else:
-                        for letter in word:
-                                if letter in punc: 
-                                    word = word.replace(letter, "") 
-                        if word in vector_words:
-                            cleanComments[i].append(word)
-
-        data = zip(times, subreddits, (cleanComments))
-        f = open("corpora/" + corporaDir + "_clean/" + filename + ".csv", "w")
-        writer = csv.writer(f)
-        writer.writerows(data)
+            data = zip(times, subreddits, (cleanComments))
+            f = open("corpora/" + corporaDir + "_clean/" + filename + ".csv", "w")
+            writer = csv.writer(f)
+            writer.writerows(data)
 
 
         # for i in range(len(cleanComments)):
         #     print("Subreddit:\t" + subreddits[i] + "\nTime:\t" + str(times[i]) + "\nComment:\n" + str(cleanComments[i]))
-     
+    if includeStopWords:
+        if not os.path.isdir("./corpora/" + corporaDir + "_clean_stops"):
+            os.mkdir("./corpora/" + corporaDir + "_clean_stops")
+    else:
+        if not os.path.isdir("./corpora/" + corporaDir + "_clean"):
+            os.mkdir("./corpora/" + corporaDir + "_clean")
+    
+    with open("./helperFiles/" + vectorWordsFile, "rt") as f:
+        fin = f.read()
+        vector_words = set(fin.split())
+    f.close
+
     i = 1 
-    for filename in os.listdir('./corpora/' + corporaDir):
+    for filename in os.listdir(prePath + corporaDir):
             if filename.endswith(".txt"):
-                if (i%1 == 0):
+                if (i%10 == 0):
                     print("--- %s seconds ---" % (time.time() - start_time))
                     print ("Running file " + str(i) + ": " + filename)
                 runFile(corporaDir, filename)
@@ -185,9 +193,10 @@ def cleanCorpora(corporaDir, vectorWordsFile, includeStopWords):
 
 
 ### Generate file containing list of vector words ###
-# genVectorWords("50_corpora", 150000, True)
+# genVectorWords("/volumes/Robbie_External_Hard_Drive/", "10_corpora", 150000, True)
+# genVectorWords("/volumes/Robbie_External_Hard_Drive/", "10_corpora", 150000, False)
 
 ### Generate new, cleaned corpora ###
-cleanCorpora("10_corpora", "150000_words_stops_removed", False)
+cleanCorpora("/volumes/Robbie_External_Hard_Drive/", "10_corpora", "vector_words_150000_derived_10_corpora_stops.txt", True)
 
-#NOTE: Still need to fix else case for stops included
+#NOTE: Still need to fix else case for stops included, add .txt
