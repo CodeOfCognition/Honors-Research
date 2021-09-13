20b550b3808fb6275d91904b025caa4e4ddb6a34
CodeOfCognition <59781260+CodeOfCognition@users.noreply.github.com>
2021-09-12 22:51:59 -0400

    updated for 4 quantiles

--- shortIUWF.py
+++ shortIUWF.py
@@ -20,40 +20,43 @@ def runFile(corpusDir, corpus):
     dfData = dfData.sort_values('time', ascending=(True)).reset_index()
     numWords = 0 # word counter for each quantile
     start = 0 # index of first row in quantile
-    data = ["", "", "", "", "", "", "", "", "", "", "", ""] # word data for each quantile
+    data = [""] # word data for each quantile
     times = list() # list of time ranges of each quantile
     q = 0 # index of current quantile 
     for index, row in dfData.iterrows():
-        if numWords < 100000:
+        if numWords < 25000:
             data[q] += dfData['comment'][index]
             numWords += dfData['wc'][index]
         else:
-            q += 1
-            numWords = 0
+            data.append("")
             times.append(dfData['time'][index-1] - dfData['time'][start])
-    q -= 1
-    smallest = 0
-    for i in range(1, q+1):
-        if times[i] < times[smallest]:
-            smallest = i
-    if times[smallest] <= 31536000:
-        with open("/volumes/Robbie_External_Hard_Drive/shortIUWF/" + corpus + ".txt", "wt") as f:
-            f.write(str(times[smallest]) + "\n")
-            f.write(data[smallest])
+            start = index
+            numWords = 0
+            q += 1
+    q -= 1 # adjusted to indicate the total number quantiles created
+    smallest = [0,1,2,3]
+    
+    for i in range(1, (q+1)-3): #range goes 1 more than number of quantiles minus (# of quantiles per 100,000 words - 1)
+        if times[i] + times[i+1] + times[i+2] + times[i+3] < times[smallest[0]] + times[smallest[1]] + times[smallest[2]] + times[smallest[3]]:
+            smallest = [i, i+1, i+2, i+3]
+    if times[smallest[0]] + times[smallest[1]] + times[smallest[2]] + times[smallest[3]] <= 31536000/2:
+        with open("/volumes/Robbie_External_Hard_Drive/shortIUWF/" + corpus[0:-4] + ".txt", "wt") as f:
+            # f.write(str(times[smallest[0]] + times[smallest[1]] + times[smallest[2]] + times[smallest[3]]) + "\n")
+            f.write(data[smallest[0]] + data[smallest[1]] + data[smallest[2]] + data[smallest[3]])
     #now we know what the smallest time is. We should check if it's smaller than a year, add it to a new file and at the top of the files say how big it is.
 
  
 
 
 
-def run(prePath, corporaDir):
+def run(prePath, corporaDir): 
 
     i = 1
     for filename in os.listdir(prePath + corporaDir):
             if filename.endswith(".csv"):
-                if (i%1 == 0):
+                if (i%10 == 0):
                     print("--- %s seconds ---" % (time.time() - start_time))
-                if (i%1 == 0):
+                if (i%10 == 0):
                     print("running file " + str(i) + ": " + filename)
                 runFile((prePath + corporaDir + '/'), filename)
                 i += 1
